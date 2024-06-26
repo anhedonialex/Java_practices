44da4263c4fc80c1eac76bd0d32e0d5d1ad8001a
Xek0mb1k <84407094+Xek0mb1k@users.noreply.github.com>
2024-06-26 19:22:39 +0300

    task 8 complete

--- exam/even/test/src/main/java/org/example/Main.java
+++ exam/even/test/src/main/java/org/example/Main.java
@@ -1,27 +1,40 @@
 package org.example;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.math.BigInteger;
 import java.util.*;
 
-public class Main {
+public class Main extends Thread {
+    private final int number;
+    private BigInteger factorial = BigInteger.valueOf(1);
+
+
+    public Main(int number) {
+        this.number = number;
+    }
+
+    public void run() {
+        if (number < 0) {
+            System.out.println("Факториал отрицательного числа не определен");
+            return;
+        }
+
+        int i = 1;
+        while (i <= number) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+            i++;
+        }
+
+        System.out.println("Факториал числа " + number + " равен: " + factorial);
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите путь к файлу для сохранения объекта: ");
-        String filePath = scanner.nextLine();
+        System.out.print("Введите число для вычисления факториала: ");
+        int inputNumber = scanner.nextInt();
 
-        Message message = new Message("Alice", "Bob", "Hello, how are you?");
-
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            outputStream.writeObject(message);
-            System.out.println("Сообщение успешно сериализовано в файл: " + filePath);
-        } catch (IOException e) {
-            System.out.println("Ошибка при записи файла: " + e.getMessage());
-        }
-
-        scanner.close();
+        Main factorialThread = new Main(inputNumber);
+        factorialThread.start();
     }
 }

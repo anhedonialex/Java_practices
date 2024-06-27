f1d2a0561e555cf75ea8ebe2e96bf15ff34f644a
anhedonialex <anhedonialex@gmail.com>
2024-06-28 01:24:51 +0300

    Add E11

--- exam/odd/src/main/java/org/example/E11Calc/app/src/main/java/com/example/e11calc/MainActivity.java
+++ exam/odd/src/main/java/org/example/E11Calc/app/src/main/java/com/example/e11calc/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.e11calc;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText e1 = findViewById(R.id.editText1);
+        EditText e2 = findViewById(R.id.editText2);
+        TextView tv = findViewById(R.id.textView);
+        Button sum = findViewById(R.id.button);
+        sum.setOnClickListener(v->{
+            Log.i("Hint", "Кнопка нажата");
+            String e1_val = e1.getText().toString();
+            String e2_val = e2.getText().toString();
+            if (!e1_val.isEmpty() && !e2_val.isEmpty()){
+                int result = Integer.parseInt(e1_val) + Integer.parseInt(e2_val);
+                Log.i("Sum", String.valueOf(result));
+                tv.setText(String.valueOf(result));
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Вы ввели кринж", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
